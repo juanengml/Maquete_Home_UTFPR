d85bafc904da5960805d065e36cf4695c477c040
juanengml <juanengml@gmail.com>
2019-03-28 00:08:35 -0300

    Update

---
+++ code_shield_arduino/controle.ino
@@ -0,0 +1,85 @@
+int led1 = 13;
+int led2 = 12;
+int led3 = 11;
+int led4 = 10;
+
+int led5 = 9;
+int led6 = 8;
+int led7 = 7;
+int led8 = 2;
+
+float s_presenca = A0;
+float s_luz = A1;
+
+void setup() {
+  // put your setup code here, to run once:
+   pinMode(led1,OUTPUT);
+   pinMode(led2,OUTPUT);
+   pinMode(led3,OUTPUT);
+   pinMode(led4,OUTPUT);
+
+   pinMode(s_presenca,INPUT);
+   pinMode(s_luz,INPUT);
+   
+   pinMode(led5,OUTPUT);
+   pinMode(led6,OUTPUT);
+   pinMode(led7,OUTPUT);
+   pinMode(led8,OUTPUT);
+   Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  testLeds();
+  testSensors();
+ 
+}
+
+void testSensors(){
+  float presenca;
+  float luz;
+
+  presenca = analogRead(s_presenca);
+// luz      = map(analogRead(s_luz),0,1023, 0,255);
+  Serial.print("Sensor de Presenca: ");
+  Serial.print(presenca);
+ // Serial.print("\nSensor de Luz: ");
+//  Serial.print(luz);
+  Serial.print("\n");
+  delay(200);
+}
+
+void testLeds(){
+  digitalWrite(led1,HIGH);
+  delay(500);
+  digitalWrite(led2,HIGH);
+  delay(500);
+  digitalWrite(led3,HIGH);
+  delay(500);
+  digitalWrite(led4,HIGH);
+  delay(500);
+  digitalWrite(led5,HIGH);
+  delay(500);
+  digitalWrite(led6,HIGH);
+  delay(500);
+  digitalWrite(led7,HIGH);
+  delay(500);
+  digitalWrite(led8,HIGH);
+  delay(1000);
+  digitalWrite(led8,LOW);
+  delay(500);
+  digitalWrite(led7,LOW);
+  delay(500);
+  digitalWrite(led6,LOW);
+  delay(500);
+  digitalWrite(led5,LOW);
+  delay(500);
+  digitalWrite(led4,LOW);
+  delay(500);
+  digitalWrite(led3,LOW);
+  delay(500);
+  digitalWrite(led2,LOW);
+  delay(500);
+  digitalWrite(led1,LOW);
+  delay(1000);
+}
---
+++ code_shield_arduino/controle2.ino
@@ -0,0 +1,66 @@
+int led1 = 13;
+
+int leds[7] =   {12,11,10,9,8,7,2};
+int status[7] = {0 ,0 ,0 ,0,0,0,0};
+
+int s_presenca = A0;
+float s_luz = A1;
+
+void controle(int pino,int opcao);
+
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(9600);
+   pinMode(led1,OUTPUT);
+   int i = 0;
+   for(i = 0; i < 7; i++){
+    Serial.print(leds[i]);
+    Serial.print(": ");
+    Serial.print(status[i]);
+    Serial.print("\n");
+    pinMode(leds[i],OUTPUT);
+   }
+  
+   pinMode(s_presenca,INPUT);
+   pinMode(s_luz,INPUT);
+   
+   
+}
+
+void loop() {
+  // checa presensa e mudar status led
+  Serial.print("Sensor de Presenca: ");
+  Serial.print(status_presenca());
+  Serial.print("\n");
+  
+  // Controle de leds
+  controle(12,1);
+  delay(1000);
+  controle(7,0);
+  delay(1000);
+}
+
+void controle(int pino,int opcao){
+   int i = 0;
+   for(i = 0; i < 7;i++){
+     if (leds[i] == pino){
+      status[i] = opcao;
+      digitalWrite(leds[i],opcao);
+     }
+     Serial.print(leds[i]);
+     Serial.print(": ");
+     Serial.print(status[i]);
+     Serial.print("\n");
+   }
+}
+
+
+int status_presenca()
+ {if (analogRead(s_presenca) == 1){
+     digitalWrite(led1,HIGH);
+     return 1;
+  }else{
+    digitalWrite(led1,LOW);
+    return 0;
+  }
+ }
